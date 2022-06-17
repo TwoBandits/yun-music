3d786bb6fcd87862c44263d44e0479734e86ed28
Cml <1484254946@qq.com>
2022-06-18 00:05:08 +0800

    创建项目，集成uView-ui框架

---
+++ components/y-tabbar/y-tabbar.vue
@@ -0,0 +1,13 @@
+<template>
+	<view class="y-tabbar-container">
+		<u-tabbar>
+			<u-tabbar-item ></u-tabbar-item>
+		</u-tabbar>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="find-content">
+		<view class="test">
+			<u-tabbar>
+				<u-tabbar-item text="发现"></u-tabbar-item>
+				<u-tabbar-item text="我的"></u-tabbar-item>
+			</u-tabbar>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
